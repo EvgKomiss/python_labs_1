9f19b759e89b8e5a1daa490b979a63ed99647d43
KereK DereK <evgeny.komissarov95@yandex.ru>
2023-01-14 02:43:17 +0400

    now using pandas

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,21 +1,22 @@
-import csv
+import pandas
 from datetime import datetime
 
 
-def second_task():
-    years = []
-    dates = {}
-    with open("../Lab1/dataset.csv", 'r', newline='') as file:
-        reader = csv.reader(file, delimiter=' ', quotechar='|')
-        for row in reader:
-            date = str(row).split(",")[0][2:].replace("-", '')
-            year = date[:4]
-            if year not in years:
-                years.append(str(row).split(",")[0][2:].split("-")[0])
-                dates[year] = []
-            dates[year].append(date)
-        print(years[1:])
+def first_task(df: pandas.DataFrame):
+    pass
+
+
+def second_task(df: pandas.DataFrame):
+    pass
+
+
+def third_task(df: pandas.DataFrame):
+    pass
+
 
 
 if __name__ == '__main__':
-    second_task()
+    dataframe = pandas.read_csv("../Lab1/dataset.csv")
+    first_task(dataframe)
+    second_task(dataframe)
+    third_task(dataframe)
