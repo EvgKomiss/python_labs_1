6fbac20dce2da0f2d552b9934551160764927342
KereK DereK <evgeny.komissarov95@yandex.ru>
2023-01-20 17:21:13 +0400

    fourth task done + some changes to read_data func

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -3,7 +3,11 @@ import matplotlib
 
 
 def read_data(path: str = '../Lab1/dataset.csv') -> pandas.DataFrame:
-    return pandas.read_csv(path)
+    df = pandas.read_csv(path)
+    df = drop_invalid_rows(df)
+    df['Date'] = pandas.to_datetime(df['Date'], format='%Y-%m-%d')
+    df['Value'] = pandas.to_numeric(df['Value'])
+    return df
 
 
 def rename_columns(df: pandas.DataFrame) -> pandas.DataFrame:
@@ -14,5 +18,11 @@ def drop_invalid_rows(df: pandas.DataFrame) -> pandas.DataFrame:
     return df.replace(to_replace='None', value=pandas.NaT).replace(to_replace="NaT", value=pandas.NaT).dropna()
 
 
+def create_value_deviation_columns(df: pandas.DataFrame) -> pandas.DataFrame:
+    df['median_dev'] = df['value'] - df['value'].median()
+    df['mean_dev'] = df['value'] - df['value'].mean()
+    return df
+
+
 if __name__ == '__main__':
-    print(drop_invalid_rows(rename_columns(read_data())))
+    print(create_value_deviation_columns(rename_columns(read_data())))
