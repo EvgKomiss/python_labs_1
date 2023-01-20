71ff95f5e77ff1c09fdaad63ee4c071cdbee38a9
KereK DereK <evgeny.komissarov95@yandex.ru>
2023-01-20 16:49:01 +0400

    second task done

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -2,9 +2,13 @@ import pandas
 import matplotlib
 
 
-def read_data(path: str = '../Lab1/dataset.csv'):
+def read_data(path: str = '../Lab1/dataset.csv') -> pandas.DataFrame:
     return pandas.read_csv(path)
 
 
+def rename_columns(df: pandas.DataFrame) -> pandas.DataFrame:
+    return df.rename(columns={'Date': 'date', 'Value': 'value'})
+
+
 if __name__ == '__main__':
-    print(read_data())
+    print(rename_columns(read_data()))
