572c60b1822601a8c31cb72b6f087f1647bf90c7
KereK DereK <evgeny.komissarov95@yandex.ru>
2023-01-20 19:43:03 +0400

    tenth task done

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import pandas
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 matplotlib.use('TkAgg')
 
 
@@ -41,16 +42,33 @@ def filter_by_date(df: pandas.DataFrame, start_date: datetime.date, end_date: da
     return df.loc[df['date'].between(start_date, end_date)]
 
 
-def group_by_month(df: pandas.DataFrame) -> pandas.DataFrame:
+def group_by_month_mean(df: pandas.DataFrame) -> pandas.DataFrame:
     return df.groupby(df['date'].dt.month)['value'].mean()
 
 
-def draw_global_chart(df: pandas.DataFrame) -> None:
+def group_by_month_median(df: pandas.DataFrame) -> pandas.DataFrame:
+    return df.groupby(df['date'].dt.month)['value'].median()
+
+
+def draw_global_chart(df: pandas.DataFrame, title: str = '') -> None:
     plt.figure('Global value chart')
-    plt.plot(df['date'], df['mean_dev'], color="green")
+    plt.plot(df['date'], df['value'], color="green")
     plt.xlabel('Date')
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gcf().autofmt_xdate()
     plt.ylabel('Value')
+    plt.title(title, loc='left')
     plt.show()
+
+
+def draw_monthly_chart(df: pandas.DataFrame, month: int) -> None:
+    df = df[df['date'].dt.month == month]
+    dfs = [df[df['date'].dt.year == y] for y in df['date'].dt.year.unique()]
+    for df in dfs:
+        mean = group_by_month_mean(df).reset_index()['value'].tolist()[0]
+        median = group_by_month_median(df).reset_index()['value'].tolist()[0]
+        title = "Mean: " + str(mean) + "    Median: " + str(median)
+        draw_global_chart(df, title)
 
 
 if __name__ == '__main__':
@@ -58,5 +76,6 @@ if __name__ == '__main__':
     write_statistics(new_df)
     print(filter_by_deviation(new_df, 2.37))
     print(filter_by_date(new_df, datetime(2022, 10, 12), datetime(2022, 10, 14)))
-    print(group_by_month(new_df))
+    print(group_by_month_mean(new_df))
     draw_global_chart(new_df)
+    draw_monthly_chart(new_df, 11)
