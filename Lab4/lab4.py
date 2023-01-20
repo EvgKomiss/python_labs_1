807d21461cb24491ed950b192ccc4540fbb7f0a4
KereK DereK <evgeny.komissarov95@yandex.ru>
2023-01-20 17:54:42 +0400

    seventh task done

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas
 import matplotlib
 
@@ -5,7 +6,7 @@ import matplotlib
 def read_data(path: str = '../Lab1/dataset.csv') -> pandas.DataFrame:
     df = pandas.read_csv(path)
     df = drop_invalid_rows(df)
-    df['Date'] = pandas.to_datetime(df['Date'], format='%Y-%m-%d')
+    df['Date'] = pandas.to_datetime(df['Date'], format='%Y-%m-%d',)
     df['Value'] = pandas.to_numeric(df['Value'])
     return df
 
@@ -34,7 +35,12 @@ def filter_by_deviation(df: pandas.DataFrame, deviation: float or int) -> pandas
     return df[df['mean_dev'] >= deviation]
 
 
+def filter_by_date(df: pandas.DataFrame, start_date: datetime.date, end_date: datetime.date):
+    return df.loc[df['date'].between(start_date, end_date)]
+
+
 if __name__ == '__main__':
     new_df = create_value_deviation_columns(rename_columns(read_data()))
     write_statistics(new_df)
     print(filter_by_deviation(new_df, 2.37))
+    print(filter_by_date(new_df, datetime(2022, 10, 12), datetime(2022, 10, 14)))
