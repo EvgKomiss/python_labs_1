54d23ee32882a46420570ece72165722d91c8195
KereK DereK <evgeny.komissarov95@yandex.ru>
2023-01-20 17:06:04 +0400

    third task done

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -10,5 +10,9 @@ def rename_columns(df: pandas.DataFrame) -> pandas.DataFrame:
     return df.rename(columns={'Date': 'date', 'Value': 'value'})
 
 
+def drop_invalid_rows(df: pandas.DataFrame) -> pandas.DataFrame:
+    return df.replace(to_replace='None', value=pandas.NaT).replace(to_replace="NaT", value=pandas.NaT).dropna()
+
+
 if __name__ == '__main__':
-    print(rename_columns(read_data()))
+    print(drop_invalid_rows(rename_columns(read_data())))
